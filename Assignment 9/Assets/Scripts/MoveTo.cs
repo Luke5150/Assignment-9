0b1495c704d52dc953848f01df35b79df5b9c9e6
Luke5150 <lhensley@mail.bradley.edu>
2022-05-12 18:32:32 -0500

    Assignment 9

---
+++ Assignment 9/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * (Luke Hensley)
+ * (Assignment 9)
+ * (Makes the player move to a goals)
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+
+    public Transform goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
